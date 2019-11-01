ccb85a774fc9fd838e5c66b55b7cc065c278e41d
Sebastian Peters <Sebastian.Peters@gmail.com>
2019-11-01 13:01:56 +0100

    Add editorconfig and associated plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.1.9.RELEASE"
     id("io.spring.dependency-management") version "1.0.8.RELEASE"
+    id("org.ec4j.editorconfig") version "0.0.3"
     kotlin("jvm") version "1.3.50"
     kotlin("plugin.spring") version "1.3.50"
 }
